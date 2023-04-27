3cd52963c3c00b3d6d1592868b8f68de71b235eb
Shitaanshus27 <shitaanshus27@gmail.com>
2023-04-27 19:58:11 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (request, response) {
-  response.send(`<h1>Hello,Calc</h1>`);
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (request, response) {
+  var number1 = Number(request.body.num1);
+  var number2 = Number(request.body.num2);
+  var result = number1 + number2;
+  response.send("<b>Result of Number1 and Number 2</b>" + "    " + result);
 });
 
 app.listen(3000, () => {
